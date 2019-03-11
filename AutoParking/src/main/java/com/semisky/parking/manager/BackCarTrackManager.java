91374c0c6efd21e47a3cf2ac29e18fc7d4208888
liuyong <liuyong@semisky.com>
2019-03-11 11:31:56 +0800

    修改问题：轨迹中间角度值最大值

--- AutoParking/src/main/java/com/semisky/parking/manager/BackCarTrackManager.java
+++ AutoParking/src/main/java/com/semisky/parking/manager/BackCarTrackManager.java
@@ -8,7 +8,7 @@ public class BackCarTrackManager {
     public static final int TRACK_ANGEL_MAX = 15600;// 最大倒车轨迹角度值
     public static final int TRACK_ANGLE_STEP = 156;// 轨迹角度最小单元
     public static final int MIDDLE_TRACK_ANGLE_MIN = (TRACK_ANGEL_MAX / 2) - TRACK_ANGLE_STEP;// 轨迹中间角度值最小值
-    public static final int MIDDLE_TRACK_ANGLE_MAX = (TRACK_ANGEL_MAX / 2) - TRACK_ANGLE_STEP;// 轨迹中间角度值最大值
+    public static final int MIDDLE_TRACK_ANGLE_MAX = (TRACK_ANGEL_MAX / 2) + TRACK_ANGLE_STEP;// 轨迹中间角度值最大值
 
     public static final int TYPE_TRACE_LEFT = 1;
     public static final int TYPE_TRACE_MIDDLE = 2;
@@ -45,22 +45,22 @@ public class BackCarTrackManager {
 
 
     public void handlerBackCarTraceData(int trackAngle) {
-        if(trackAngle < 0){
+        if (trackAngle < 0) {
             trackAngle = 0;
         }
-        if(trackAngle > TRACK_ANGEL_MAX){
+        if (trackAngle > TRACK_ANGEL_MAX) {
             trackAngle = TRACK_ANGEL_MAX;
         }
 
-        if(trackAngle >= MIDDLE_TRACK_ANGLE_MIN || trackAngle <= MIDDLE_TRACK_ANGLE_MAX){
+        if (trackAngle >= MIDDLE_TRACK_ANGLE_MIN || trackAngle <= MIDDLE_TRACK_ANGLE_MAX) {
             // 中间倒车轨迹角度
-            notifyBackCarTrackChanged(TYPE_TRACE_MIDDLE,1);
-        }else if(trackAngle < MIDDLE_TRACK_ANGLE_MIN){
+            notifyBackCarTrackChanged(TYPE_TRACE_MIDDLE, 1);
+        } else if (trackAngle < MIDDLE_TRACK_ANGLE_MIN) {
             // 左边倒车轨迹角度
-            notifyBackCarTrackChanged(TYPE_TRACE_LEFT,trackAngle/TRACK_ANGLE_STEP);
-        }else if(trackAngle > MIDDLE_TRACK_ANGLE_MAX){
+            notifyBackCarTrackChanged(TYPE_TRACE_LEFT, trackAngle / TRACK_ANGLE_STEP);
+        } else if (trackAngle > MIDDLE_TRACK_ANGLE_MAX) {
             // 右边倒车轨迹角度
-            notifyBackCarTrackChanged(TYPE_TRACE_RIGHT,(trackAngle-TRACK_ANGEL_MAX/2)/156);
+            notifyBackCarTrackChanged(TYPE_TRACE_RIGHT, (trackAngle - TRACK_ANGEL_MAX / 2) / 156);
         }
     }
 
